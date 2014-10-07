31561059731c433736477ea8b407d790151b8a53
Oisin Grehan <oising@gmail.com>
2014-10-07 17:23:43 -0500

    finished dynamicmethodbinder and added tests

---
+++ Async.Tests/TestDynamicMethodBinder.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nivot.PowerShell.Async;
+
+namespace Async.Tests
+{
+    public class Dummy
+    {
+        public static void SimpleAction(Action action)
+        {
+            action();
+        }
+
+        public static T SimpleFunc<T>(Func<T> func)
+        {
+            return func();
+        }
+
+        public void GenericA<T>(Action<T> action, T arg)
+        {
+            action(arg);
+        }
+
+        public TResult GenericA<T1, TResult>(Func<T1, TResult> func, T1 arg1)
+        {
+            return func(arg1);
+        }
+
+        public TResult GenericA<T1, T2, TResult>(Func<T1, T2, TResult> func, T1 arg1, T2 arg2)
+        {
+            return func(arg1, arg2);
+        }
+
+        public static void GenericB<T>(Action<T> action, T arg)
+        {
+            action(arg);
+        }
+
+        public static TResult GenericB<T1, TResult>(Func<T1, TResult> func, T1 arg1)
+        {
+            return func(arg1);
+        }
+
+        public static TResult GenericB<T1, T2, TResult>(Func<T1, T2, TResult> func, T1 arg1, T2 arg2)
+        {
+            return func(arg1, arg2);
+        }
+    }
+
+    [TestClass]
+    public class TestDynamicMethodBinder
+    {
+
+        [TestMethod]
+        public void SimpleInstanceInvoke()
+        {
+            var builder = new StringBuilder("Hello, ");            
+            var result = DynamicMethodBinder.Invoke<object>(builder, "Append", false, "World.");
+            Assert.IsInstanceOfType(result, typeof(StringBuilder));
+            Assert.IsTrue(result.ToString() == "Hello, World.");
+        }
+
+        [TestMethod]
+        public void SimpleInstanceDiscardedInvoke()
+        {
+            var val = 0;
+            Func<int> func = () => val = 42;
+            DynamicMethodBinder.InvokeNoResult(func, "Invoke", false);
+            Assert.IsTrue(val == 42);
+        }
+        [TestMethod]
+        public void SimpleInstanceVoidInvoke()
+        {
+            var val = 0;
+            Action action = () => val = 42;
+            DynamicMethodBinder.InvokeNoResult(action, "Invoke", false);
+            Assert.IsTrue(val == 42);
+        }
+
+        [TestMethod]
+        public void SimpleStaticInvoke()
+        {
+            string result = DynamicMethodBinder.Invoke<string>(
+                typeof (String), "Format", true, "Hello, {0}.", "World");
+            Assert.IsTrue(result == "Hello, World.");
+        }
+
+        [TestMethod]
+        public void SimpleStaticDiscardedInvoke()
+        {
+            DynamicMethodBinder.InvokeNoResult(
+                typeof(String), "Format", true, "Hello, {0}.", "World");
+        }
+
+        [TestMethod]
+        public void SimpleStaticVoidInvoke()
+        {
+            int value = 0;
+            Action action = () => value = 42;
+            DynamicMethodBinder.InvokeNoResult(typeof(Dummy), "SimpleAction", true, action);
+            Assert.IsTrue(value == 42);
+        }
+
+        [TestMethod]
+        public void GenericInstanceInvoke()
+        {
+            var d = new Dummy();
+            int value = 1;
+            Func<int, int> func2 = incr => value + incr;
+            var result = DynamicMethodBinder.Invoke<int>(d, "GenericA", false, func2, 41);
+            Assert.IsTrue(result == 42);
+
+            // try different overload
+            Func<int, int, int> func3 = (incr1, incr2) => value + incr1 + incr2;
+            result = DynamicMethodBinder.Invoke<int>(d, "GenericA", false, func3, 20, 21);
+            Assert.IsTrue(result == 42);
+        }
+
+        [TestMethod]
+        public void GenericInstanceDiscardedInvoke()
+        {
+            var d = new Dummy();
+            int value = 1;
+            Func<int, int> func = incr => value = value + incr;
+            DynamicMethodBinder.InvokeNoResult(d, "GenericA", false, func, 41);
+            Assert.IsTrue(value == 42);
+        }
+
+        [TestMethod]
+        public void GenericInstanceVoidInvoke()
+        {
+            var d = new Dummy();
+            int value = 1;
+            Action<int> action = incr => value = value + incr;
+            DynamicMethodBinder.InvokeNoResult(d, "GenericA", false, action, 41);
+            Assert.IsTrue(value == 42);
+        }
+
+        [TestMethod]
+        public void GenericStaticInvoke()
+        {
+            int value = 1;
+            Func<int, int> func2 = incr => value + incr;
+            var result = DynamicMethodBinder.Invoke<int>(typeof(Dummy), "GenericB", true, func2, 41);
+            Assert.IsTrue(result == 42);
+
+            // try different overload
+            Func<int, int, int> func3 = (incr1, incr2) => value + incr1 + incr2;
+            result = DynamicMethodBinder.Invoke<int>(typeof(Dummy), "GenericB", true, func3, 20, 21);
+            Assert.IsTrue(result == 42);
+        }
+
+        [TestMethod]
+        public void GenericStaticDiscardedInvoke()
+        {
+            int value = 1;
+            Func<int, int> func2 = incr => value = value + incr;
+            DynamicMethodBinder.InvokeNoResult(typeof(Dummy), "GenericB", true, func2, 41);
+            Assert.IsTrue(value == 42);
+        }
+
+        [TestMethod]
+        public void GenericStaticVoidInvoke()
+        {            
+            int value = 1;
+            Action<int> action = incr => value = value + incr;
+            DynamicMethodBinder.InvokeNoResult(typeof(Dummy), "GenericB", true, action, 41);
+            Assert.IsTrue(value == 42);
+        }
+    }
+}
---
+++ AsyncCallJobSource/DynamicMethodBinder.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using Microsoft.CSharp.RuntimeBinder;
+using Sigil;
+using Binder = Microsoft.CSharp.RuntimeBinder.Binder;
+
+namespace Nivot.PowerShell.Async
+{
+    public static class DynamicMethodBinder
+    {
+        // NOTE: Funcs/Actions with an arity of `1 to `9 are in System.dll,
+        //       but `10 to `16 are [currently] in System.Core.dll
+        private static readonly Lazy<IEnumerable<Type>> DelegateTypesLazy =
+            new Lazy<IEnumerable<Type>>(
+                () => (typeof (Func<>).Assembly.ExportedTypes
+                    .Union(typeof (Func<,,,,,,,,,,>).Assembly.ExportedTypes))
+                    .Where(f => f.IsGenericTypeDefinition &&
+                        typeof (MulticastDelegate).IsAssignableFrom(f)));
+
+        /// <summary>
+        /// Invoke a method by name when you expect to receive a return a value. This method will
+        /// fail if the late-bound method is of void return type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="target"></param>
+        /// <param name="methodName"></param>
+        /// <param name="staticContext"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static T Invoke<T>(object target, [NotNull] string methodName, bool staticContext, params object[] parameters)
+        {
+            return (T) Invoke(target, methodName,
+                staticContext: staticContext, wantReturnValue: true, parameters: parameters);
+        }
+
+        /// <summary>
+        /// Invoke a method by name when you don't want, or expect, to receive a return a value. This
+        /// method can invoke both void and non-void late bound targets.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="methodName"></param>
+        /// <param name="staticContext"></param>
+        /// <param name="parameters"></param>
+        public static void InvokeNoResult(object target, [NotNull] string methodName, bool staticContext, params object[] parameters)
+        {
+            Invoke(target, methodName,
+                staticContext: staticContext, wantReturnValue: false, parameters: parameters);
+        }
+
+        private static object Invoke([NotNull] object target, [NotNull] string methodName, bool staticContext, bool wantReturnValue, params object[] parameters)
+        {
+            if (target == null) throw new ArgumentNullException("target");
+            if (methodName == null) throw new ArgumentNullException("methodName");
+
+            // This is the magic sauce that lets the DLR bind static method invocations
+            // dynamically. The current C# 5 compiler (I don't know about Rosyln) does not
+            // support this:
+            //
+            //     dynamic t = typeof(Console)
+            //     t.Beep()
+            //
+            // This will fail at runtime complaining that TypeInfo does not contain a method
+            // named Beep. By specifying the flags below, this will succeed. I presume this was
+            // cut due to shipping pressure. 
+            var binderArgs = new List<CSharpArgumentInfo>
+            {
+                (staticContext) ?
+                    CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.IsStaticType |
+                        CSharpArgumentInfoFlags.UseCompileTimeType, null) :
+                    CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null)
+            };
+
+            // This is mostly just lip service for the C# binder as we really don't care about the
+            // distinction between null references, numeric/string/null literals and const types
+            // as everything is coming in - at runtime - boxed/byref in the parameters array to
+            // this method. Binding of the target method should succeed regardless. There are a
+            // few scenarios where these details matter - out/ref parameters for example. A null
+            // literal being passed would be invalid for a ref/out; this would need a null ref.
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                //object parameter = parameters[i];
+                
+                binderArgs.Add(CSharpArgumentInfo.Create(
+                    CSharpArgumentInfoFlags.UseCompileTimeType, null));
+            }
+
+            var callSite = BuildCallSite(methodName, wantReturnValue, parameters, binderArgs);
+
+            // TODO: build this with expression tree and cache compiled output?
+            Func<object> fn =
+                () =>
+                    ((Delegate) callSite.Target).DynamicInvoke(
+                        (new object[] {callSite, target}).Concat(parameters).ToArray());
+
+            return fn();
+        }
+
+        private static dynamic BuildCallSite(string methodName, bool wantReturnValue, object[] parameters, List<CSharpArgumentInfo> binderArgs)
+        {
+            // ResultDiscarded is an optimization that the compiler can make at compile time that we
+            // cannot determine at runtime. That is to say, I have no idea if the caller will save the
+            // result of the invocation (if there is any). This flag has nothing to do with the return
+            // type of the target method in this case. If the target bound method's return type actually
+            // is void then this is neccessary or we get "Cannot implicitly convert type 'void' to 'object'"
+            CallSiteBinder binder = Binder.InvokeMember(
+                wantReturnValue ? CSharpBinderFlags.None : CSharpBinderFlags.ResultDiscarded,
+                methodName,
+                null,
+                typeof (DynamicMethodBinder), // The purpose of this ref is unclear to me.
+                binderArgs);
+
+            var typeArgs = new List<Type>
+            {
+                typeof (CallSite),
+                typeof (object)
+            };
+
+            if (parameters.Length > 0)
+            {
+                typeArgs.AddRange(Type.GetTypeArray(parameters)); // FIXME: fails if parameters contains a null
+            }
+
+            if (wantReturnValue)
+            {
+                typeArgs.Add(typeof (object));
+            }
+
+            // returns open generic type Func`n/Action`n
+            Type callSiteParameterType = GetFuncOrActionTypeByArity(wantReturnValue, typeArgs.ToArray());
+            Type callSiteType = typeof (CallSite<>).MakeGenericType(callSiteParameterType);
+
+            MethodInfo create = callSiteType.GetMethod("Create", new[] {typeof (CallSiteBinder)});
+            dynamic callSite = create.Invoke(callSiteType, new object[] {binder});
+            
+            return callSite;
+        }
+
+        private static Type GetFuncOrActionTypeByArity(bool hasReturnType, params Type[] typeArguments)
+        {
+            // yeah, hacky, but simpler than a giant switch with 32 type literals
+            // and probably faster than repeated use of GetType(...)
+            string delegateTypeName = hasReturnType ? "Func" : "Action";
+            
+            Type delegateType = (from fn in DelegateTypesLazy.Value
+                where (fn.Name.StartsWith(delegateTypeName)) // Func`1, Func`2, ... etc.
+                      && (fn.GetGenericArguments().Length == typeArguments.Length)
+                select fn)
+                .SingleOrDefault();
+
+            if (delegateType == null)
+            {
+                throw new ArgumentOutOfRangeException("typeArguments",
+                    "Unable to determine a suitable delegate for the given generic type arguments.");
+            }
+
+            return delegateType.MakeGenericType(typeArguments);
+        }
+
+
+    }
+}
